2a42a5110e6210ec196a09fb68a51d7a3d810347
Carlos Mijail Mamani Anccasi <89269010+CharlsMikhail@users.noreply.github.com>
2026-06-11 00:07:14 -0500

    Change app port from 80 to 8081

--- app.py
+++ app.py
@@ -51,4 +51,4 @@ def transaccion():
     return jsonify({"error": "Token invalido"}), 401
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=80, debug=True)
+    app.run(host="0.0.0.0", port=8081, debug=True)
